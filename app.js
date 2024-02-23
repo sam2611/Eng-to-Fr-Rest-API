62e190b0227f924df091c003f138b4d3d8f4a233
Sakshi Mishra <sakshimishra2611@gmail.com>
2024-02-23 23:39:34 +0530

    root

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the translation API.');
+});
+
 app.post('/translate', async (req, res) => {
   try {
     const { text } = req.body;
